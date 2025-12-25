1423885a239fd794e856ef05aa5864bfc307e5ef
Bhanu Pratap <bhanubhardwaj9801@gmail.com>
2025-12-25 21:31:17 +0530

    Enable CORS for Railway deployment

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import engine
 from app.models import Base
 from app.routers import router
@@ -8,7 +8,13 @@ from app.routers import router
 app = FastAPI(title="Internship Operations Management System")
 
 Base.metadata.create_all(bind=engine)
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # later you can restrict
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
 
 app.mount("/frontend", StaticFiles(directory="frontend", html=True), name="frontend")
